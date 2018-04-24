9a5bbdaaa1ab7b1855527a7f3fd26bdd261ac875
Lekan Nicholson <lekan.nicholson@gmail.com>
2018-04-24 00:49:19 +0000

    automatically backed up by learn

--- config/routes.rb
+++ config/routes.rb
@@ -53,5 +53,5 @@ Rails.application.routes.draw do
   #     # (app/controllers/admin/products_controller.rb)
   #     resources :products
   #   end
-  get 'post/:id', to: 'posts#show'
+  get 'posts/:id', to: 'posts#show'
 end
